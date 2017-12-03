a6ae99296f8203dbefc07dc7de200c2705e5b63e
Ivascu Adrian <ivascuadrian9@gmail.com>
2017-12-03 23:18:28 +0100

    Rimosso il mazzo e aggiunto il deck nel Giocatore.java

--- src/battleroyale/Giocatore.java
+++ src/battleroyale/Giocatore.java
@@ -8,14 +8,14 @@ public class Giocatore
 {
 	public String nomeGiocatore;
 	public int gold;
-	public ArrayList<Carta> mazzo;
 	public Carta[] carteSbloccate;
+	public Carta[] deck;
 	
-	public Giocatore(String nomeGiocatore, int gold, ArrayList<Carta> mazzo, Carta[] carteSbloccate)
+	public Giocatore(String nomeGiocatore, int gold, Carta[] carteSbloccate, Carta[] deck)
 	{
 		this.nomeGiocatore = nomeGiocatore;
 		this.gold = gold;
-		this.mazzo = mazzo;
 		this.carteSbloccate = carteSbloccate;
+		this.deck = deck;
 	}
 }
