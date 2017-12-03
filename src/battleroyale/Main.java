fd751f80563df833783947fef030df30c5e97990
Ivascu Adrian <ivascuadrian9@gmail.com>
2017-12-03 23:18:59 +0100

    Menu di base nel Main.java

--- src/battleroyale/Main.java
+++ src/battleroyale/Main.java
@@ -1,5 +1,8 @@
 package battleroyale;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main
 {
     public static void main(String[] args)
@@ -10,68 +13,89 @@ public class Main
         Giocatore gg2 = new Giocatore("Scino", 50, null, null);
         
         Partita partita = new Partita(new Giocatore[] {gg1, gg2});
-
-    	partita.aggiungiCarta(0);
-    	partita.aggiungiCarta(1);
-    	partita.aggiungiCarta(3);
-    	
-    	partita.cambiaTurno();
-
-    	partita.aggiungiCarta(0);
-    	partita.aggiungiCarta(1);
-    	partita.aggiungiCarta(3);
-    	
-    	partita.cambiaTurno();
-
-    	partita.aggiungiCarta(0);
-    	partita.aggiungiCarta(1);
-    	partita.aggiungiCarta(3);
-    	
-    	partita.cambiaTurno();
-
-    	partita.aggiungiCarta(0);
-    	partita.aggiungiCarta(1);
-    	partita.aggiungiCarta(3);
-    	
-    	partita.cambiaTurno();
-
-    	partita.aggiungiCarta(0);
-    	partita.aggiungiCarta(1);
-    	partita.aggiungiCarta(3);
-    	
-    	partita.cambiaTurno();
-
-    	partita.aggiungiCarta(0);
-    	partita.aggiungiCarta(1);
-    	partita.aggiungiCarta(3);
-    	
-    	partita.cambiaTurno();
-
-    	partita.aggiungiCarta(0);
-    	partita.aggiungiCarta(1);
-    	partita.aggiungiCarta(3);
-    	
-    	partita.cambiaTurno();
-
-    	partita.aggiungiCarta(0);
-    	partita.aggiungiCarta(1);
-    	partita.aggiungiCarta(3);
-    	
-    	partita.cambiaTurno();
-
-    	partita.aggiungiCarta(0);
-    	partita.aggiungiCarta(1);
-    	partita.aggiungiCarta(3);
-    	
-    	partita.cambiaTurno();
-    	
-    	partita.attacca(0, 0);
-    	
-    	partita.cambiaTurno();
-    	
-    	partita.attacca(0, 0);
-    	
-    	partita.cambiaTurno();
-    	
+        
+        BufferedReader tast = new BufferedReader(new InputStreamReader(System.in));
+        char scelta;
+        
+        do
+        {
+        	try
+        	{
+        		System.out.print("-- Menu --\n"
+        				+ "[1] Cambia turno\n"
+        				+ "[2] Riepilogo\n"
+        				+ "[3] Attacca\n"
+        				+ "[4] Aggiungi carta nel campo da battaglia\n"
+        				+ "[5] Mostra mazzo\n"
+        				+ "\nScelta: ");
+        		scelta = tast.readLine().charAt(0);
+        		
+        		switch(scelta)
+        		{
+        			case '1':
+        				partita.cambiaTurno();
+        				break;
+        			case '2':
+        				partita.riepilogoPartita();
+        				break;
+        			case '3':
+        				if(partita.carteNelCampo[partita.turno].size() > 0)
+        				{
+        					partita.mostraMazzo(partita.turno);
+        					
+        					System.out.print("Scegli la tua carta (0 per annullare attacco)"
+        	        				+ "\nScelta: ");
+        					scelta = tast.readLine().charAt(0);
+        					
+        					if(scelta == '0')
+        					{
+        						scelta = '3'; // Per ritornare al menu di prima
+        						break;
+        					}
+        					else
+        					{
+        						int cartaAtt = Integer.parseInt("" + scelta);
+        						
+        						int avversario = partita.turno + 1 == partita.NUM_GG ? 0 : partita.turno + 1;
+        						partita.mostraMazzo(avversario);
+            					
+            					System.out.print("Scegli la carta da attaccare (0 per annullare attacco)"
+            	        				+ "\nScelta: ");
+            					scelta = tast.readLine().charAt(0);
+            					
+            					if(scelta == '0')
+            					{
+            						scelta = '3'; // Per ritornare al menu di prima
+            						break;
+            					}
+            					else
+            					{
+            						int cartaAvv = Integer.parseInt("" + scelta);
+            						partita.attacca(cartaAtt, cartaAvv);
+            					}
+        					}
+        				}
+        				else
+        				{
+        					System.out.println("Non hai nessuna carta nel campo di battaglia");
+        				}
+        				break;
+        			case '4':
+        				
+        				break;
+        			case '5':
+        				partita.mostraMazzo(partita.turno);
+        				break;
+        			default:
+        				
+        				break;
+        		}
+        	}
+        	catch(Exception e)
+        	{
+        		System.out.println(e);
+        	}
+        }
+        while(true);
     }
 }
